6ca10f89ee5cd7616730892dc0b6a84006a499ff
Alex Cuba <alexcubamchs19@gmail.com>
2025-04-02 14:38:07 -0400

    Updated Images

--- packages/dart/npt_flutter/lib/styles/app_color.dart
+++ packages/dart/npt_flutter/lib/styles/app_color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const primaryColor = Color(0xFFF05E3E);
+  static const primaryColor = Color(0xFF0E4370);
   static const surfaceColor = Color(0xFFF2F2F2);
   static const dividerColor = Color(0xFFC4C4C4);
   static const onSurfaceColor = Color(0xFF747474);
